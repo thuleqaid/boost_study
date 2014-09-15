591587f619a25c20aaf4d923797bc528ecc5eca4
ThuleQaid <thuleqaid@debian>
2014-09-15 15:57:26 +0800

    demo:regex v2

--- demo-regex/entry/main.cpp
+++ demo-regex/entry/main.cpp
@@ -27,6 +27,32 @@ void test_it2()
 		i++;
 	}
 }
+void test_base()
+{
+	boost::regex pat(R"((?<addr>\d{6})(?<century>\d{2})(?<year>\d{2})(?<month>\d{2})(?<day>\d{2})(?<idx>\d{3})(?<crc>(\w)|(\d)))");
+	std::string idnumber="12345620140102012X";
+	boost::smatch match;
+	bool b_match=boost::regex_search(idnumber,match,pat);
+	if (b_match)
+	{
+		LOG(info)<<match.size();
+		for(boost::smatch::size_type i=0;i<match.size();++i)
+		{
+			LOG(info)<<"Pos("<<i<<"):"<<match.position(i);
+			/* use group index to select sub-match result */
+			auto item=match[i];
+			LOG(info)<<"  Match:"<<item.matched;
+			LOG(info)<<"  Value:"<<item.str();
+			LOG(info)<<"  Length:"<<item.length();
+			LOG(info)<<"  From("<<std::distance(idnumber.cbegin(),item.first)<<") To("<<std::distance(idnumber.cbegin(),item.second)<<")";
+		}
+		/* use group name to select sub-match result */
+		auto item=match["birth"];
+		LOG(info)<<item.str();
+		std::string id_15bit=boost::regex_replace(idnumber,pat,"$+{addr}${3}$+{month}$+{day}$+{idx}");
+		LOG(info)<<id_15bit;
+	}
+}
 int main(int argc, char **argv)
 {
 	loginit(LEVEL_TRACE);
@@ -38,5 +64,6 @@ int main(int argc, char **argv)
 	//LOG(fatal)<< "fatal message";
 	test_it1();
 	test_it2();
+	test_base();
 	return 0;
 }
