fc06dd7f099e761129fe9a61859516cecfe11e64
ThuleQaid <thuleqaid@163.com>
2015-05-03 06:58:22 +0800

    remove calendar translate projects

--- lunar/main.cmake
+++
@@ -1,6 +0,0 @@
-# Setting for Source/Header files
-set(SRCROOT "${ROOT_DIR}")
-#file(GLOB SOURCE ${SRCROOT}/src/*.cpp)
-aux_source_directory(${SRCROOT}/src SOURCE)
-aux_source_directory(${SRCROOT}/entry ENTRY)
-include_directories(${SRCROOT}/include)
